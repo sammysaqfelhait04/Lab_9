41f7f46c73b6438432bbc8fa3e02cdbc07e75e2d
sammysaqfelhait04 <ssaqfelhait@student.cscc.edu>
2026-03-13 15:09:54 -0400

    Add files via upload

---
+++ unit9/player.py
@@ -0,0 +1,33 @@
+"""
+ Player Class
+Sammy Saqfelhait
+This  represents a player who has coins and a coin object to toss.
+03/13/2026
+"""
+
+from coin import Coin
+
+class Player:
+
+    def __init__(self, name):
+        self.__name = name
+        self.__wallet = 20
+        self.__coin = Coin()
+
+    def toss_coin(self):
+        self.__coin.toss()
+
+    def get_coin_side(self):
+        return self.__coin.get_sideup()
+
+    def win_coin(self):
+        self.__wallet += 1
+
+    def lose_coin(self):
+        self.__wallet -= 1
+
+    def get_wallet(self):
+        return self.__wallet
+
+    def get_name(self):
+        return self.__name
